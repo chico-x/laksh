4de45cdab35eca0efa7746b5c0e33a6519a8bfe2
overachiever <chicostudyat@gmail.com>
2025-02-24 23:40:06 +0530

    Added some lines to the script
    Need to add some more lines

--- laksh.sh
+++ laksh.sh
@@ -7,3 +7,22 @@ sudo apt update -y
 # Upgrade installed packages
 echo "Upgrading packages..."
 sudo apt upgrade -y
+
+# install zerotier
+echo "installing zerotier...."
+curl -s https://install.zerotier.com/ | bash
+
+#join zerotier network
+echo "Joining zerotier network...."
+zerotier-cli join 565799d8f6bca37c
+
+# install python dependencies
+echo "installing python dependencies...."
+apt-get install git python3-virtualenv libssl-dev libffi-dev build-essential libpython3-dev python3-minimal authbind virtualenv
+
+# Add user 'cowrie' with disabled password
+adduser --disabled-password --gecos "" cowrie
+
+# Switch to user 'cowrie'
+su - cowrie
+
