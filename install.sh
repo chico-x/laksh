4b1ec01ffa145572cf77a8d51ce6a143ab261006
Athuldev Menon <athuldev4721@gmail.com>
2025-02-28 15:01:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,3 @@
-#remove the sleep commands if you think it's unnecessary
-#i just added it for better debugging of the script
-
-#!/bin/bash
-
-# Update package list
-echo "Updating package list..."
-sleep 2
-apt-get update -y
-
-# Upgrade installed packages
-echo "Upgrading packages..."
-sleep 2
-apt-get upgrade -y
-
-# Install ZeroTier
-echo "Installing ZeroTier..."
-sleep 2
-apt install curl -y
-curl -s https://install.zerotier.com/ | bash
-
-# Join ZeroTier network
-echo "Joining ZeroTier network..."
-sleep 2
-zerotier-cli join  db64858fed998184
 
 # Install Python dependencies
 echo "Installing Python dependencies..."
